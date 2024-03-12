f1f6f58203c24abe101ecb5d205528b70b559c36
Martin Crabtree <martin.crabtree@gmail.com>
2024-03-12 15:46:22 -0400

    added basic layout and added base message submission code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,59 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import styled from "styled-components";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "@chatscope/chat-ui-kit-react-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const LayoutContainer = styled.div`
+  position: relative;
+  height: 720px;
+  width: 1280px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello world!",
+      sender: "This is the Sender",
+    },
+  ]);
 
-  return <div>app</div>;
+  const _renderMessages = (message) => {
+    return <Message key={uuid()} model={message} />;
+  };
+
+  const _handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing", // display messages entered by user on the right
+    };
+
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+  };
+
+  return (
+    <LayoutContainer>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>{_renderMessages()}</MessageList>
+          <MessageInput
+            placeholder="Type your message here."
+            onSend={_handleSend}
+          />
+        </ChatContainer>
+      </MainContainer>
+    </LayoutContainer>
+  );
 }
 
 export default App;
