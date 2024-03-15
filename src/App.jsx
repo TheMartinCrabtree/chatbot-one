658bdb07ec63ca9b0434349294e1f10e38aebbc1
Martin Crabtree <martin.crabtree@gmail.com>
2024-03-15 17:04:36 -0400

    testing fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ import {
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
 
-const testkey = import.meta.env.VITE_TEST_KEY;
-console.log("testkey", testkey);
+const API_KEY = import.meta.env.VITE_TEST_KEY;
 
 const LayoutContainer = styled.div`
   position: relative;
@@ -51,7 +50,44 @@ const App = () => {
     await _getChatGPTUpdate(newMessages);
   };
 
-  const _getChatGPTUpdate = async (chatMessages) => {};
+  const _getChatGPTUpdate = async (chatMessages) => {
+    // notes: object differences in chat/api messages arrays
+    // chatMessages { sender: "user" or "ChatGPT", message: "message content" }
+    // apiMessages { role: "user" or "assistant", content: "message content" }
+    // have to transmute array from chatMessages to apiMessages before sending to API
+    let apiMessages = chatMessages.map((messageObj) => {
+      let newMessageObj = {
+        content: messageObj.message,
+      };
+      newMessageObj.role =
+        messageObj.sender === "ChatGPT" ? "assistant" : "user";
+
+      return newMessageObj;
+    });
+
+    // prefix role to set style of GPT response
+    const systemMessage = {
+      role: "system",
+      content: "Explain all concepts in the tone of a pirate."
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions",{
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data)=>{return data.json()}.then((data)=>console.log("api returned data: ", data)));
+  };
 
   return (
     <LayoutContainer>
